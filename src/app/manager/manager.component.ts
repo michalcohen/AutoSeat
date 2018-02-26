562889fd76ce50bd5995eb9b8691c03b824d0392
Michal Cohen <cohenmelany1@gmail.com>
2018-02-26 13:08:38 +0200

    invitee counter

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -18,6 +18,7 @@ export class ManagerComponent implements OnInit {
     public invitedState: ISeats;
     public displayedColumns: string[];
     public dataSource: MatTableDataSource<ISeat>;
+    public amountArrived: number = 0;
 
     constructor(public SimpleSeatsManagerService: SeatsManagerService) { }
 
@@ -34,8 +35,14 @@ export class ManagerComponent implements OnInit {
     public loadTable() {
         this.SimpleSeatsManagerService.getData().subscribe(data => {
             this.invitedState = data;
-            this.displayedColumns = ['name', 'tableNumber', 'hasArrived'];
+            this.displayedColumns = ['name', 'tableNumber', 'amount', 'hasArrived'];
             this.dataSource = new MatTableDataSource(this.invitedState.guests);
+            this.amountArrived = 0;
+            this.invitedState.guests.forEach(element => {
+                if (element.hasArrived){
+                    this.amountArrived += element.amount;
+                }
+            });
         }, data => {
             console.log(data);
         });
@@ -51,32 +58,3 @@ export class ManagerComponent implements OnInit {
     }
 }
 
-export interface Element {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-    { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-    { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-    { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-    { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-    { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-    { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-    { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-    { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-    { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-    { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
--- src/app/welcome/seats-manager/ISeat.ts
+++ src/app/welcome/seats-manager/ISeat.ts
@@ -1,5 +1,6 @@
 export interface ISeat {
     name: string
     tableNumber: number
+    amount: number
     hasArrived: boolean
 }
